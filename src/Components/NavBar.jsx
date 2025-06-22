8a6196f7d54c7e2d025833b5822bde3bb8e9b9eb
JAWAD018 <mohmmedjawad36@gmail.com>
2025-06-22 16:37:54 +0530

    About Page

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -37,9 +37,9 @@ function NavBar() {
       {/* Mobile Menu */}
       {isOpen && (
         <div className="md:hidden bg-[#F9FAFB] px-4 py-2 space-y-2 font-semibold text-[#221E1E]">
-          <a href="#" className="block  py-1  ">Home</a>
-          <a href="#" className="block py-1  ">About</a>
-          <a href="#" className="block py-1  ">Contact Us</a>
+          <a href="/" className="block  py-1  ">Home</a>
+          <a href="/about" className="block py-1  ">About</a>
+          <a href="contact" className="block py-1  ">Contact Us</a>
           <a href="/login" className="block bg-[#7C3AED] text-white px-3 py-1 rounded-lg w-fit">Login</a>
         </div>
       )}
